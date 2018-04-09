24863d7565b5277b4e8ec03d544c7ed4c90475a8
Diego Bazin <diegbazin@gmail.com>
2018-04-09 13:52:36 +0200

    modif code

---
+++ tp3.2.js
@@ -0,0 +1,20 @@
+
+
+function validation(){
+	testchamp();
+	nom();
+	age();
+	tel();
+	birth();
+}
+
+function testchamp()
+{
+	if ((document.getElementById("nom").value == "") ||
+		(document.getElementById("age").value == "") ||
+		(document.getElementById("telephone").value == "") ||
+		(document.getElementById("naissance").value == ""))
+	{
+		alert("Champ vide");
+	}
+}
